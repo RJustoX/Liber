614eafc7066da7c1d778300cee765456b6503d84
RJustoX <rafael.justoscherer@gmail.com>
2021-08-05 19:23:44 -0300

    :boom: Nascimento do projeto :boom:

---
+++ android/app/src/main/kotlin/com/example/nicotine/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nicotine
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Container(
+        color: Colors.indigo[900],
+      ),
+    );
+  }
+}
